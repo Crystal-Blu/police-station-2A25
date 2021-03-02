c1042aee7a61a7a00444c5a641ada484def6a418
tazarkour <jaouani.walid@esprit.tn>
2021-03-02 17:55:36 +0100

    first commit of the qt design

---
+++ mainwindow.cpp
@@ -0,0 +1,21 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    ui->tabWidget_2->setCurrentIndex(ui->tabWidget_2->count()-2);
+
+}
